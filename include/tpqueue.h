3a46444bcef9ae48a088c36a84806d261faf780b
PRIGARINA-VICTORIA <99180874+PRIGARINA-VICTORIA@users.noreply.github.com>
2022-05-01 21:28:15 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -4,61 +4,60 @@
 
 template<typename T>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
-    private:
-	struct ITEM {
-		T data;
-		ITEM* next;
-	};
-	ITEM* start_s;
+ private:
+    struct ITEM {
+        T data;
+        ITEM* next;
+    };
+    ITEM* start_s;
 
-   public:
-	TPQueue():start_s(nullptr) {}
-	void push(T value) {
-		ITEM* start = start_s;
-		if (start == nullptr) {
-			start = new ITEM;
-			start_s = start;
-			start->data = value;
-			start->next = nullptr;
-		} else {
-			ITEM* temp = new ITEM;
-			temp->data = value;
-			if (start_s->data.prior < temp->data.prior) {
-				temp->next = start;
-				start_s = temp;
-				return;
-			}
-			while (start != nullptr) {
-				if ((start->data.prior) >= (temp->data.prior) &&
-				    ((start->next) == nullptr ||
-				     (((start->data.prior) < (temp->data.prior)) ||
-				      ((start->next) != nullptr &&
-				       start->next->data.prior < (temp->data.prior))))) {
-					temp->next = start->next;
-					start->next = temp;
-					return;
-				}
-				start = start->next;
-			}
-		}
-	}
-	T pop() {
-		ITEM* start = nullptr;
-		start = start_s;
-		if (start != nullptr) {
-			T rem = start->data;
-			start_s = start->next;
-			return rem;
-		} else {
-			throw "Empty!";
-		}
-	}
+ public:
+    TPQueue():start_s(nullptr) {}
+    void push(T value) {
+        ITEM* start = start_s;
+        if (start == nullptr) {
+            start = new ITEM;
+            start_s = start;
+            start->data = value;
+            start->next = nullptr;
+        } else {
+            ITEM* temp = new ITEM;
+            temp->data = value;
+            if (start_s->data.prior < temp->data.prior) {
+                temp->next = start;
+                start_s = temp;
+                return;
+            }
+            while (start != nullptr) {
+                if ((start->data.prior) >= (temp->data.prior) &&
+                    ((start->next) == nullptr ||
+                     (((start->data.prior) < (temp->data.prior)) ||
+                      ((start->next) != nullptr &&
+                       start->next->data.prior < (temp->data.prior))))) {
+                    temp->next = start->next;
+                    start->next = temp;
+                    return;
+                }
+                start = start->next;
+            }
+        }
+    }
+    T pop() {
+        ITEM* start = nullptr;
+        start = start_s;
+        if (start != nullptr) {
+            T rem = start->data;
+            start_s = start->next;
+            return rem;
+        } else {
+            throw "Empty!";
+        }
+    }
 };
 
 struct SYM {
-  char ch;
-  int prior;
+    char ch;
+    int prior;
 };
 
 #endif  // INCLUDE_TPQUEUE_H_
